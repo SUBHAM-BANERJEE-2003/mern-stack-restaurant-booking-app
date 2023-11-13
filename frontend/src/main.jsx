81dcb972b5fddd11d61d0df060c8bef58f7e914b
SUBHAM BANERJEE <subhambanerjee2003@gmail.com>
2023-11-13 20:59:22 +0530

    changed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -14,21 +14,12 @@ import ReservationPage from '../pages/ReservationPage.jsx'
 import Createaccount from '../pages/Createaccount.jsx'
 import { UserProvider } from './Usercontext.jsx' 
 import UserProfile from '../src/components/UserProfile.jsx'
-import background from '../public/background.jpg';
 const router = createBrowserRouter([
   {
     path: '/',
     element: (
       <UserProvider>
-        <div style={{
-          display: "block",
-          backgroundImage: `url(${background})`,
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "100% 100%",
-          maxWidth: "100%",
-          maxHeight: "100%",
-          opacity: 0.8,
-        }}><App /></div>
+        <App />
       </UserProvider>
     ),
   },
