718a3fc33fbb09c0f0c6060d761eb07cc0cc9b1f
SUBHAM BANERJEE <subhambanerjee2003@gmail.com>
2024-02-22 17:44:29 +0530

    made table responsive

--- frontend/pages/ReservationPage.jsx
+++ frontend/pages/ReservationPage.jsx
@@ -21,36 +21,32 @@ function ReservationPage() {
   return (
     <>
       <Navbar />
-      <div className="min-h-screen bg-gray-100 py-5" style={{ fontFamily: 'Oxygen', fontWeight: 'bold' }}>
-        <div className="w-full max-w-7xl mx-auto px-4 mt-10">
-          <div className="w-full mb-8 overflow-hidden rounded-lg shadow-lg">
-            <div className="w-full overflow-x-auto">
-              <table id="task-tbl" className="task-tbl w-full">
-                <thead>
-                  <tr className="text-md font-semibold tracking-wide text-left text-gray-900 bg-gray-100 uppercase border-b border-gray-600">
-                    <th className="px-4 py-3">Registration #</th>
-                    <th className="px-4 py-3">Booking Name</th>
-                    <th className="px-4 py-3">Booking Day</th>
-                    <th className="px-4 py-3">Booking Date</th>
-                    <th className="px-4 py-3">Booking time slot</th>
-                    <th className="px-4 py-3">Status</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {reservations.map((reservation, index) => (
-                    <tr key={index}>
-                      <td className="px-4 py-3">{index + 1}</td>
-                      <td className="px-4 py-3">{reservation.username}</td>
-                      <td className="px-4 py-3">{reservation.Day}</td>
-                      <td className="px-4 py-3">{reservation.Datetime}</td>
-                      <td className="px-4 py-3">{reservation.Timeslot}</td>
-                      <td className="px-4 py-3">{reservation.Status}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
-          </div>
+      <div className="bg-gray-100 py-5" style={{ fontFamily: 'Oxygen', fontWeight: 'bold' }}>
+        <div class='overflow-x'>
+          <table class='table-auto overflow-scroll w-full bg-gray-100'>
+            <thead>
+              <tr className="text-md font-semibold tracking-wide text-left text-gray-900 bg-gray-100 uppercase border-b border-gray-600">
+                <th className="px-4 py-3 w-1/6">Registration #</th>
+                <th className="px-4 py-3 w-1/6">Booking Name</th>
+                <th className="px-4 py-3 w-1/6">Booking Day</th>
+                <th className="px-4 py-3 w-1/6">Booking Date</th>
+                <th className="px-4 py-3 w-1/6">Booking time slot</th>
+                <th className="px-4 py-3 w-1/6">Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              {reservations.map((reservation, index) => (
+                <tr key={index}>
+                  <td className="px-4 py-3">{index + 1}</td>
+                  <td className="px-4 py-3">{reservation.username}</td>
+                  <td className="px-4 py-3">{reservation.Day}</td>
+                  <td className="px-4 py-3">{reservation.Datetime}</td>
+                  <td className="px-4 py-3">{reservation.Timeslot}</td>
+                  <td className="px-4 py-3">{reservation.Status}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
       <Footer />
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer className="bg-gray-800 text-white py-4 mt-8 ">
+    <footer className="bg-gray-800 text-white py-4 mt-8 w-full fixed bottom-0 left-0">
       <div className="container mx-auto flex items-center justify-center">
         <div className="border-r border-white pr-4 mr-4"></div>
-        <div className="text-sm" style={{ fontFamily: 'Oxygen', fontWeight: 'bold'}}>
+        <div className="text-sm" style={{ fontFamily: 'Oxygen', fontWeight: 'bold' }}>
           &copy; 2023 Dailycious Restaurant. All Rights Reserved.
         </div>
       </div>
     </footer>
+
   );
 }
 
